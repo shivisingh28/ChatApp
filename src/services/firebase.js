2e92f9b248df96be3deab33cca635b5d9229f865
shivisingh28 <shivi2819997@gmail.com>
2020-09-02 16:39:55 +0530

    First commit

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCCgKJETvgDay_ZcqyPAvVWiEn0XuDGp30",
+    authDomain: "webapp-a8591.firebaseapp.com",
+    databaseURL: "https://webapp-a8591.firebaseio.com",
+    projectId: "webapp-a8591",
+    storageBucket: "webapp-a8591.appspot.com",
+    messagingSenderId: "1025999118476",
+    appId: "1:1025999118476:web:f778381745dc00cea21ba9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
